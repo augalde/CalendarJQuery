5e8773301d6344e565c1ff703aa7848f1f419421
augalde <augalde@gmail.com>
2018-04-16 13:43:23 -0600

    Adding holidays hard coded

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # CalendarJQuery
 Task requeste by Number8
+
+# a. list of any requirements you could not implement.
+		Holidays: incompleted implementation: hardcoded list of days representing the holidays
+# b. Bugs you’ve identified but didn’t have time to fix.
+		None. Just quick tested the app.
+# c. Things you would do if you had more time to complete the task.
+		Holidays: I would connect the app to a google calendar API to get holidays per country and validate agains country code.
+#Please indicate the time it took you to complete the solutions
+(MANDATORY).
+		Sunday: research about datePicker: 1.5 hours
+		Monday: From 8:30 to 1:00pm. development and research for holidays: 4 hours and lunch :)
+		
+Research: google.com
--- calendar.js
+++ calendar.js
@@ -1,21 +1,76 @@
 $(document).ready(function(){	
+    
+//holidays hardcoded
+var holidays = [
+    [1, 1, 'uk'],
+    [1, 2, 'uk'],
+    [1, 3, 'uk'],
+    [1, 4, 'uk'],
+    [12, 24, 'uk'],
+    [12, 25, 'uk'],
+    [12, 26, 'uk'],
+    [12, 27, 'uk'],
+    [12, 28, 'uk'],
+    [12, 29, 'uk'],
+    [12, 30, 'uk'],
+    [12, 31, 'uk'],
+    [1, 1, 'us'],
+    [1, 15, 'us'],
+    [2, 19, 'us'],
+    [5, 28, 'us'],
+    [7, 04, 'us'],
+    [7, 18, 'us'],
+    [09, 03, 'us'],
+    [11, 22, 'us'],
+    [11, 23, 'us'],
+    [12, 24, 'us'],
+    [12, 25, 'us'],
+    [12, 31, 'us'],
+    [1, 1, 'cr'],
+    [3, 29, 'cr'],
+    [3, 30, 'cr'],
+    [4, 11, 'cr'],
+    [5, 1, 'cr'],
+    [7, 25, 'cr'],
+    [08, 2, 'cr'],
+    [08, 15, 'cr'],
+    [09, 15, 'cr'],
+    [10, 12, 'cr'],
+    [12, 25, 'cr']
+  ];
 
+  //TODO: Change this for an API calendar from google to validate holidays from all over the world
+  //validating holidays from an array
+  function isHoliday(dateObject, countryCode) {
+    for(var i = 0; i < holidays.length; i++) {
+        let localDay = parseInt(dateObject.getDate());
+        let localMonth = parseInt(dateObject.getMonth()) + 1;
+      if (localDay == parseInt(holidays[i][1]) &&  localMonth == parseInt(holidays[i][0]) && countryCode.toLowerCase() == holidays[i][2].toLowerCase()) {
+                return true;
+      }
+    }
+    
+    return false;
+  }
+
+    //setting the dates
     $("#timeSelect").change(function () {
         var dateSelected = new Date($("#timeSelect").val());
         var fullDate = (dateSelected.getMonth()+1)+"/"+(dateSelected.getDate()+1)+"/" + dateSelected.getFullYear();
             $("#datepicker").val(fullDate);
         });
 
+        //building the calendar picker
         $("#datepicker").datepicker({
             showOn: "button",
             buttonImage: "https://jqueryui.com/resources/demos/datepicker/images/calendar.gif",
             buttonImageOnly: true,
             buttonText: "Select date",
             
-            //rendering the calendar
+            //rendering the calendar using datePicker
             beforeShowDay: function(date) {
                 var eventDates = {};
-
+            var countryCode = $("#countryCode").val();
             var dateSelected = new Date($("#timeSelect").val());
             var fullDate = (dateSelected.getMonth()+1)+"/"+(dateSelected.getDate()+1)+"/" + dateSelected.getFullYear();
             var dias = $("#numberOfDays").val();            
@@ -35,6 +90,10 @@ $(document).ready(function(){
                         {
                             return [true, "weekend", 'Tooltip text'];
                         }
+                        else if(isHoliday(myDay,countryCode.toLowerCase()))
+                        {
+                            return [true, "holiday", 'Tooltip text'];
+                        }                
                         else{
                          return [true, "selectedDays", 'Tooltip text'];
                         }
