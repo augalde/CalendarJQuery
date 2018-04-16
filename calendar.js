308fc726fabcbc04a757fe31edfb5511a55b3ab0
augalde <augalde@gmail.com>
2018-04-16 10:41:43 -0600

    Selecting the dates in the picker and gray out the other ones (not need it)

--- calendar.js
+++ calendar.js
@@ -10,5 +10,30 @@ $(document).ready(function(){
             showOn: "button",
             buttonImage: "https://jqueryui.com/resources/demos/datepicker/images/calendar.gif",
             buttonImageOnly: true,
-            buttonText: "Select date"});
-});
+            buttonText: "Select date",
+            
+            beforeShowDay: function(date) {
+                var eventDates = {};
+
+            var dateSelected = new Date($("#timeSelect").val());
+            var fullDate = (dateSelected.getMonth()+1)+"/"+(dateSelected.getDate()+1)+"/" + dateSelected.getFullYear();
+            var dias = $("#numberOfDays").val();            
+            
+            for(var i=0; i<dias;i++){
+                var dayToPaint= new Date(fullDate);
+                dayToPaint.setDate(dayToPaint.getDate()+i);
+                var newFecha = (dayToPaint.getMonth() + 1)+ '/' +dayToPaint.getDate()+ '/' + dayToPaint.getFullYear();
+                eventDates[ new Date(newFecha)] = new Date(newFecha);
+            }
+                    
+                    var highlight = eventDates[date];
+                    if( highlight ) {
+                         return [true, "selectedDays", 'Tooltip text'];
+                    } else {
+                          return [true, "nonValidDays", 'Tooltip text'];
+                    }
+                }
+            });
+        
+        });
+
