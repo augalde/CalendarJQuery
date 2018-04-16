25fa116e925c2aa2bd4a4cdcc8606a6545c0f8ba
augalde <augalde@gmail.com>
2018-04-16 11:09:07 -0600

    Adding weekend and fixing styles

--- calendar.js
+++ calendar.js
@@ -12,6 +12,7 @@ $(document).ready(function(){
             buttonImageOnly: true,
             buttonText: "Select date",
             
+            //rendering the calendar
             beforeShowDay: function(date) {
                 var eventDates = {};
 
@@ -19,16 +20,24 @@ $(document).ready(function(){
             var fullDate = (dateSelected.getMonth()+1)+"/"+(dateSelected.getDate()+1)+"/" + dateSelected.getFullYear();
             var dias = $("#numberOfDays").val();            
             
+            //creating the array of days we are going to paint
             for(var i=0; i<dias;i++){
                 var dayToPaint= new Date(fullDate);
                 dayToPaint.setDate(dayToPaint.getDate()+i);
                 var newFecha = (dayToPaint.getMonth() + 1)+ '/' +dayToPaint.getDate()+ '/' + dayToPaint.getFullYear();
                 eventDates[ new Date(newFecha)] = new Date(newFecha);
             }
-                    
+                    //highlight the calendar
                     var highlight = eventDates[date];
                     if( highlight ) {
+                        let myDay = new Date(highlight);
+                        if(myDay.getDay() == 0 || myDay.getDay() == 6)
+                        {
+                            return [true, "weekend", 'Tooltip text'];
+                        }
+                        else{
                          return [true, "selectedDays", 'Tooltip text'];
+                        }
                     } else {
                           return [true, "nonValidDays", 'Tooltip text'];
                     }
